e8561c84021d5e9cebb5f8493f1fbe8b0ee43f5b
Dmitry <dimkin3@gmail.com>
2025-03-09 16:41:49 +0000

    class Entity

--- entity/entity.py
+++ entity/entity.py
@@ -1,21 +1,39 @@
 class Entity:
     def __init__(self):
         self.hp = 100
+    # def __str__(self):
+    #     return f'{self.__class__.__name__}'
+
 class Rock(Entity):
     def __init__(self):
         self.speed = 0
+        self.name = 'Rock'
+        self.colour = 'gray'
 class Grass(Entity):
     def __init__(self):
         self.speed = 0
+        self.name = 'Grass'
+        self.colour = 'green'
 class Tree(Entity):
     def __init__(self):
         self.speed = 0
+        self.name = 'Tree'
+        self.colour = 'brown'
 class Creature(Entity):
     def __init__(self):
         self.speed = 0
+        self.name = 'Creature'
 class Herbivore(Creature):
     def __init__(self):
         self.speed = 0
+        self.name = 'Herb'
+        self.colour = 'yellow'
 class Predator(Creature):
     def __init__(self):
         self.speed = 0
+        self.name = 'Predator'
+        self.colour = 'red'
+class Empty(Entity):
+    def __init__(self):
+        self.name = ''
+        self.colour = 'white'
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
-#import entity
 import random
 from collections import Counter
+
+from entity.entity import Herbivore, Predator, Grass, Rock, Tree, Empty
+
+
 class Render:
 
     def __init__(self, simulation_):
@@ -10,6 +13,7 @@ class Render:
         self.root.title("simulation")
         self.map = simulation_.map.map_dict
         self.action = simulation_.action
+        self.width, self.height = simulation_.width, simulation_.height
 
         self.button_next = tk.Button(self.root, text="Обновить", command = self.on_button_click)
         self.button_next.pack(side="right", padx=10, pady=10)
@@ -23,14 +27,17 @@ class Render:
     def display_grid(self):
        # Отображение сетки
 
-        for i in range(10):
-            for j in range(10):
-                cell = tk.Label(self.frame_grid, text=f"{self.map[(i, j)]}", borderwidth=1, relief="solid", width=8, height=3)
+        for i in range(self.width):
+            for j in range(self.height):
+                entity = self.map[(i, j)]
+                color = getattr(entity, "colour", "white")  # Получаем цвет или белый
+                name = getattr(entity, "name", "")  # Получаем имя или "Empty"
+                cell = tk.Label(self.frame_grid, text=f"{name}", borderwidth=1, relief="solid", width=8, height=3, bg = color)
                 cell.grid(row=i, column=j, padx=2, pady=2)
 
     def update_label(self):
         """Обновляет текстовое поле справа"""
-        value_counts = dict(Counter(self.map.values()))
+        value_counts = Counter(v.name for v in self.map.values())
         text = '\n'.join(f"{key}: \t {value}" for key, value in value_counts.items())
 
         if hasattr(self, "label"):  # Если метка уже создана, обновляем
@@ -57,14 +64,18 @@ class Map:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.map_dict = {(x, y): None for x in range(self.width) for y in range(self.height)}
+        self.map_dict = {(x, y): Empty() for x in range(self.width) for y in range(self.height)}
 
 class Simulation:
-    def __init__(self, width, height):
+    def __init__(self, width, height, entity_):
+        self.width, self.height = width, height
+        self.entity_dict = entity_
         self.map = Map(width, height)
+
         self.action = Actions(self)
         self.action.init_actions()
         self.render = Render(self)
+
 
 
 
@@ -76,22 +87,34 @@ class Simulation:
 
 class Actions:
     def __init__(self, simulation_):
-        self.entity_dict = {'herb': 12, 'grass': 15, 'rock': 10, 'tree': 10, 'predator': 12}
+        self.entity_dict = simulation_.entity_dict
         self.map = simulation_.map.map_dict
 
     def init_actions(self):
+
         cells_fill = set(list(self.map.keys()))
         for entity_, amount in self.entity_dict.items():
             cells = set(random.sample(list(cells_fill), amount))
             for cell in cells:
-                self.map[cell] = entity_
+                match entity_:
+                    case 'herb':
+                        self.map[cell] = Herbivore()
+                    case 'grass':
+                        self.map[cell] = Grass()
+                    case 'rock':
+                        self.map[cell] = Rock()
+                    case 'predator':
+                        self.map[cell] = Predator()
+                    case 'tree':
+                        self.map[cell] = Tree()
             cells_fill = cells_fill - cells
 
     def turn_actions(self):
-        self.map[random.choice(list(self.map.keys()))] = '_____'
+        self.map[random.choice(list(self.map.keys()))] = Empty()
 
 
 # Запуск симуляции
 if __name__ == '__main__':
-    simulation = Simulation(10, 10)
+    entity_dict = {'herb': 12, 'grass': 15, 'rock': 10, 'tree': 10, 'predator': 12}
+    simulation = Simulation(15, 10, entity_dict)
     simulation.run()
